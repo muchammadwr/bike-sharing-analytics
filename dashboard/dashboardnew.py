5db4439959c7907d0429bdd7810fa7cce655016d
muchammadwr <muchammadwr@gmail.com>
2026-08-19 14:08:22 +0700

    create sidebar and kpi card

--- dashboard/dashboardnew.py
+++ dashboard/dashboardnew.py
@@ -6,10 +6,11 @@ import streamlit as st
 import plotly.express as px
 
 
-sns.set_theme(style="dark")
+# Function
+def total_rentals(df, column):
+    total = df[column].sum()
+    return total
 
-
-# KPI
 
 # Data Frame
 daily_df = pd.read_csv("daily_df.csv")
@@ -18,104 +19,68 @@ daily_demand_group_df = pd.read_csv("daily_demand_group_df.csv")
 hourly_demand_group_df = pd.read_csv("hourly_demand_group_df.csv")
 
 
-# Visualizatin
+st.set_page_config(page_title="Bike Sharing Dashboard", page_icon="🚲", layout="wide")
+st.sidebar.title("Dashboard Filters")
+
+# Sidebar
+analysis_type = st.sidebar.radio(
+    "Select Analysis Type", options=["Hourly", "Daily"], horizontal=True
+)
+
+# KPI Card
+count = total_rentals(daily_df, "count")
+registered = total_rentals(daily_df, "count")
+casual = total_rentals(daily_df, "casual")
 # Ttile
-st.title("Bike Sharing Demand Analytics", text_alignment="right")
+st.title("Bike Sharing Demand Analytics")
 st.subheader("Understanding Rental Patterns to Improve Operational Efficiency")
 
 
-total_rentals = daily_df["count"].sum()
-registered_rentals = daily_df["registered"].sum()
-casual_rentals = daily_df["casual"].sum()
-
+# KPI Card
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.metric(label="Total Rentals", value=f"{total_rentals:,.0f}")
+    st.metric(label="Total Rentals", value=f"{count:,.0f}")
 
 with col2:
-    st.metric(label="Registered", value=f"{registered_rentals:,.0f}")
+    st.metric(label="Registered", value=f"{registered:,.0f}")
 
 with col3:
-    st.metric(label="Casual", value=f"{casual_rentals:,.0f}")
+    st.metric(label="Casual", value=f"{casual:,.0f}")
 
 
-# st.markdown(
+# Chart
+# Chart
+chart_col1, chart_col2, chart_col3 = st.columns(3)
 
-#     """
-#     <div class="dashboard-title">
-#         Bike Sharing Analytics Dashboard
-#     </div>
-#     """,
-#     unsafe_allow_html=True
-# )
+demand_colors = {
+    "Low Demand": "#E74C3C",
+    "Medium Demand": "#F1C40F",
+    "High Demand": "#2ECC71",
+}
 
-# # Membaca dataset
-# hourly_grouping_df = pd.read_csv("hourly_demand_group_df.csv")
+hourly_demand_group_df = hourly_demand_group_df.sort_values("hour")
 
-# # Menampilkan data
-# st.subheader("Hourly Rental Data")
-# st.dataframe(hourly_grouping_df.head())
+hour_order = hourly_demand_group_df["hour"].tolist()
 
-# # Membuat bar chart
-# st.bar_chart(
-#     data=hourly_grouping_df,
-#     x="hour",
-#     y=["total", "average"],
-#     color="demand_group",
-#     stack=False,
-# )
+chart_col1, chart_col2, chart_col3 = st.columns(3)
 
-# import altair as alt
-# import streamlit as st
+with chart_col1:
+    fig = px.bar(
+        hourly_demand_group_df,
+        x="hour",
+        y="average",
+        color="demand_group",
+        color_discrete_map=demand_colors,
+        category_orders={"hour": hour_order},
+        title="Average Rentals by Hour",
+        labels={
+            "hour": "Hour",
+            "average": "Average Rentals",
+            "demand_group": "Demand Group",
+        },
+    )
 
+    fig.update_layout(height=400, margin=dict(l=10, r=10, t=60, b=10), showlegend=False)
 
-# # Mengurutkan data berdasarkan jam
-# hourly_grouping_df = hourly_grouping_df.sort_values("hour")
-
-# hour_order = hourly_grouping_df["hour"].tolist()
-
-# demand_colors = {
-#     "Low Demand": "#E74C3C",
-#     "Medium Demand": "#F1C40F",
-#     "High Demand": "#2ECC71",
-# }
-
-# fig = px.bar(
-#     hourly_grouping_df,
-#     x="hour",
-#     y="average",
-#     color="demand_group",
-#     color_discrete_map=demand_colors,
-#     category_orders={
-#         "hour": hour_order,
-#         "demand_group": ["Low Demand", "Medium Demand", "High Demand"],
-#     },
-#     custom_data=["total", "demand_group"],
-#     title="Average Bike Rentals by Hour",
-#     labels={
-#         "hour": "Hour",
-#         "average": "Average Rentals",
-#         "demand_group": "Demand Group",
-#     },
-# )
-
-# fig.update_traces(
-#     hovertemplate=(
-#         "<b>Hour: %{x}</b><br>"
-#         "Average Rentals: %{y:.2f}<br>"
-#         "Total Rentals: %{customdata[0]:,}<br>"
-#         "Demand Group: %{customdata[1]}"
-#         "<extra></extra>"
-#     )
-# )
-
-# fig.update_layout(
-#     height=500,
-#     bargap=0.15,
-#     xaxis_title="Hour",
-#     yaxis_title="Average Rentals",
-#     legend_title="Demand Group",
-# )
-
-# st.plotly_chart(fig, width="stretch")
+    st.plotly_chart(fig, width="stretch")
